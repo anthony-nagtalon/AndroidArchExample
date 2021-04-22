7e6092b57ebbee3f4ce48f06508cc9a0ef8c7ebd
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-04-22 16:51:29 -0700

    Created Repository

---
+++ app/src/main/java/com/example/androidarchexample/NoteRepository.kt
@@ -0,0 +1,26 @@
+package com.example.androidarchexample
+
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepository(private val noteDao: NoteDao) {
+    val allNotes: Flow<List<Note>> = noteDao.getAllNotes()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun update(note: Note) {
+        noteDao.update(note)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun delete(note: Note) {
+        noteDao.delete(note)
+    }
+}
