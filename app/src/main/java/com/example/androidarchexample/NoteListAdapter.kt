ad47a0bdbd10410d5ad95c12a36e1f3178df9523
Allen Anthony Nagtalon <ant.nagtalon22@gmail.com>
2021-04-23 12:01:39 -0700

    Created custom ListAdapter for RecyclerView

---
+++ app/src/main/java/com/example/androidarchexample/NoteListAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.androidarchexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteListAdapter : ListAdapter<Note, NoteListAdapter.NoteViewHolder>(NotesComparator()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val current = getItem(position)
+        holder.bind(current.getTitle())
+    }
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val noteItemView: TextView = itemView.findViewById(R.id.textView)
+
+        fun bind(text: String?) {
+            noteItemView.text = text
+        }
+
+        companion object {
+            fun create(parent: ViewGroup): NoteViewHolder {
+                val view: View = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.recyclerview_item, parent, false)
+                return NoteViewHolder(view)
+            }
+        }
+    }
+
+    class NotesComparator : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.getTitle() == newItem.getTitle() &&
+                    oldItem.getDescription() == newItem.getDescription()
+        }
+    }
+
+}
